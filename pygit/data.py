916306cd256b499e2126f7cee3f3da34ac44882d
NathanOnCodes <contatonathancf@gmail.com>
2024-09-26 19:35:24 -0300

    feat: set data hash func

--- pygit/data.py
+++ pygit/data.py
@@ -1,8 +1,17 @@
+import hashlib
 import os
+
 
 
 GIT_DIR = '.pygit'
 
 def init():
-    if not os.path.exists(GIT_DIR):
-        os.mkdir(GIT_DIR)
+    os.makedirs(GIT_DIR)
+    os.makedirs(f'{GIT_DIR}/objects')
+
+
+def hash_object(data):
+    oid = hashlib.sha1(data).hexdigest()
+    with open(f'{GIT_DIR}/objects/{oid}', 'wb') as out:
+        out.write(data)
+    return oid
