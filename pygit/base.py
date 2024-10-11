8dd425f8d9f0f25ce30b9b51a3528913c2ed4d04
NathanOnCodes <contatonathancf@gmail.com>
2024-10-11 18:30:19 -0300

    feat: add is_ignored func

--- pygit/base.py
+++ pygit/base.py
@@ -6,7 +6,13 @@ def write_tree(directory='.'):
     with os.scandir(directory) as it:
         for entry in it:
             full = f'{directory}/{entry.name}'
+            if is_ignored(full):
+                continue
             if entry.is_file(follow_symlinks=False):
                 print(full)
             elif entry.is_dir(follow_symlinks=False):
                 write_tree(full)
+
+
+def is_ignored(path):
+    return '.pygit' in path.split('/')
