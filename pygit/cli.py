f9a42b2171af31f6972deaf78a3ca7a353eb52c2
NathanOnCodes <contatonathancf@gmail.com>
2025-01-14 11:51:36 -0300

    feat: add oid parse_args 2

--- pygit/cli.py
+++ pygit/cli.py
@@ -27,14 +27,14 @@ def parse_args():
 
     cat_file_parser = commands.add_parser ('cat-file')
     cat_file_parser.set_defaults (func=cat_file)
-    cat_file_parser.add_argument ('object')
+    cat_file_parser.add_argument ('object', type=oid)
 
     write_tree_parser = commands.add_parser('write-tree')
     write_tree_parser.set_defaults(func=base.write_tree)
 
     read_tree_parser = commands.add_parser('read_tree')
     read_tree_parser.set_defaults(func=base.read_tree)
-    read_tree_parser.add_argument('tree')
+    read_tree_parser.add_argument('tree', type=oid)
 
     commit_parser = commands.add_parser('commit')
     commit_parser.set_defaults(func=commit)
@@ -42,15 +42,16 @@ def parse_args():
 
     log_parser = commands.add_parser('log')
     log_parser.set_defaults(func=log)
+    log_parser.add_argument('oid', type=oid, nargs='?')
 
     checkout_parser = commands.add_parser('checkout')
     checkout_parser.set_defaults(func=checkout)
-    checkout_parser.add_argument('oid')
+    checkout_parser.add_argument('oid', type=oid)
 
     tag_parser = commands.add_parser('tag')
     tag_parser.set_defaults(func=tag)
     tag_parser.add_argument('name')
-    tag_parser.add_argument('oid', nargs='?')
+    tag_parser.add_argument('oid', type=oid, nargs='?')
 
     return parser.parse_args()
 
