e0a8bcb83e3f91da67cb44d3c4cae53e978dcd39
NathanOnCodes <contatonathancf@gmail.com>
2025-01-15 17:54:40 -0300

    feat: fix refactor args oid

--- pygit/cli.py
+++ pygit/cli.py
@@ -42,11 +42,11 @@ def parse_args():
 
     log_parser = commands.add_parser('log')
     log_parser.set_defaults(func=log)
-    log_parser.add_argument('oid', type=oid, nargs='?')
+    log_parser.add_argument('oid', default='@', type=oid, nargs='?')
 
     checkout_parser = commands.add_parser('checkout')
     checkout_parser.set_defaults(func=checkout)
-    checkout_parser.add_argument('oid', type=oid)
+    checkout_parser.add_argument('oid', default='@', type=oid, nargs='?')
 
     tag_parser = commands.add_parser('tag')
     tag_parser.set_defaults(func=tag)
@@ -57,7 +57,7 @@ def parse_args():
 
 
 def init(args):
-    data.init()
+    data.init(args)
     print('Initialized empty ugit repository in %s' % os.path.join(os.getcwd(), data.GIT_DIR))
 
 
@@ -78,7 +78,7 @@ def checkout(args):
     base.checkout(args.oid)
 
 def log(args):
-    oid = args.oid or data.get_ref('HEAD')
+    oid = args.oid
     while oid:
         commit = base.get_commit(oid)
         print(textwrap.indent(commit.message, '    '))
@@ -86,8 +86,7 @@ def log(args):
         oid = commit.parent
 
 def tag(args):
-    oid = args.oid or data.get_ref('HEAD')
-    base.create_tag(args.name, oid)
+    base.create_tag(args.name, args.oid)
 
 def commit(args):
-    print(base.commit())
+    print(base.commit(args))
